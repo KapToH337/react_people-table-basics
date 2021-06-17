b575455eca22c2f0d490d4eb64ae071bb4c149c7
Artem Bazhenov <artem.bazhenov.325@mail.com>
2021-06-17 13:49:00 +0300

    Updates

---
+++ precache-manifest.84971da85c2c728035044335bab7270b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a2a2dd457e839b53c0da4948df6669",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b44b21c09dbd150394f",
+    "url": "./static/css/main.0b509bdc.chunk.css"
+  },
+  {
+    "revision": "eb5607f2e86ae3dbabcb",
+    "url": "./static/js/2.819cb57d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.819cb57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b44b21c09dbd150394f",
+    "url": "./static/js/main.8ba634cd.chunk.js"
+  },
+  {
+    "revision": "c7ae2510e9f54903a2ed",
+    "url": "./static/js/runtime-main.b51b247a.js"
+  }
+]);
